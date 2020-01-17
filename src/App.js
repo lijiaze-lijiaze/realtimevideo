fd1b3519475a55e15a9907e7034256e573da73df
lijiaze <lijiaze@outlook.com>
2020-01-17 10:22:41 +0800

    首次提交，加入了video.js的基础功能

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import videojs  from 'video.js';
 import logo from './logo.svg';
+import VideoPlayer from './videoPlayer'
 import './App.css';
 
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [{
+    src: '/path/to/video.mp4',
+    type: 'video/mp4'
+  }]
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <VideoPlayer { ...videoJsOptions } />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
